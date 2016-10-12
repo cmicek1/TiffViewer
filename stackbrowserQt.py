22ab99eff45b886958d41598776211fc13a23750
cmicek1 <cmicek325@verizon.net>
2016-10-12 17:25:25 -0400

    Created preliminary UI; app runs!

--- stackbrowserQt.py
+++ stackbrowserQt.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import PyQt4.QtCore as qc
 import PyQt4.QtGui as qg
@@ -5,9 +6,22 @@ import tiffstack as ts
 import nodedb as nd
 import edgedb as ed
 import slabdb as sd
+import stackbrowserQtUI as ui
 
 
 class MainWindow(qg.QMainWindow):
     def __init__(self):
         qg.QMainWindow.__init__(self)
-        self.setWindowTitle('StackBrowser')
+        setter = ui.Ui_MainWindow()
+        ui.Ui_MainWindow.setupUi(setter, self)
+
+
+def main():
+    app = qg.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    return app.exec_()
+
+
+if __name__ == '__main__':
+    main()
---
+++ stackbrowserQtUI.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'StackBrowserQt.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+
+
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(1032, 836)
+        MainWindow.setMouseTracking(True)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.splitter = QtGui.QSplitter(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.splitter.sizePolicy().hasHeightForWidth())
+        self.splitter.setSizePolicy(sizePolicy)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName(_fromUtf8("splitter"))
+        self.graphicsView = QtGui.QGraphicsView(self.splitter)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
+        self.graphicsView.setSizePolicy(sizePolicy)
+        self.graphicsView.setMouseTracking(True)
+        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
+        self.verticalLayout.addWidget(self.splitter)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1032, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName(_fromUtf8("menuFile"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setSizeGripEnabled(True)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar.setMouseTracking(True)
+        self.toolBar.setObjectName(_fromUtf8("toolBar"))
+        MainWindow.addToolBar(QtCore.Qt.LeftToolBarArea, self.toolBar)
+        self.toolBar_2 = QtGui.QToolBar(MainWindow)
+        self.toolBar_2.setObjectName(_fromUtf8("toolBar_2"))
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar_2)
+        self.action_Open = QtGui.QAction(MainWindow)
+        self.action_Open.setObjectName(_fromUtf8("action_Open"))
+        self.menuFile.addAction(self.action_Open)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "StackBrowser", None))
+        self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
+        self.toolBar_2.setWindowTitle(_translate("MainWindow", "toolBar_2", None))
+        self.action_Open.setText(_translate("MainWindow", "&Open...", None))
