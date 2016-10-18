119338a1cb74982e2c8ad554f7c75424debd55c6
cmicek1 <cmicek325@verizon.net>
2016-10-18 18:42:07 -0400

    Added Open function to toolbar to load stack; added click handler

--- stackbrowserQt.py
+++ stackbrowserQt.py
@@ -1,19 +1,37 @@
-import sys
+import sys, os
 import numpy as np
 import PyQt4.QtCore as qc
 import PyQt4.QtGui as qg
+import stackbrowserQtUI as ui
 import tiffstack as ts
 import nodedb as nd
 import edgedb as ed
 import slabdb as sd
-import stackbrowserQtUI as ui
+import Tkinter as Tk
+import tkFileDialog
 
 
 class MainWindow(qg.QMainWindow):
     def __init__(self):
-        qg.QMainWindow.__init__(self)
+        super(MainWindow, self).__init__()
         setter = ui.Ui_MainWindow()
         ui.Ui_MainWindow.setupUi(setter, self)
+        action_open = setter.action_Open
+
+        action_open.triggered.connect(lambda: self.click_handler('open'))
+
+    def click_handler(self, handle, *args):
+        valid_funcs = {'open': self._open}
+
+        valid_funcs[handle](args)
+
+    def _open(self, *args):
+        root = Tk.Tk()
+        root.attributes('-topmost', True)
+        root.withdraw()
+        fpath = tkFileDialog.askopenfilename(
+            initialdir=os.path.expanduser('~/Desktop'))
+        self.stack = ts.TiffStack(fpath)
 
 
 def main():
--- stackbrowserQtUI.py
+++ stackbrowserQtUI.py
@@ -30,10 +30,13 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(1032, 836)
         MainWindow.setMouseTracking(True)
+
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+
         self.splitter = QtGui.QSplitter(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -42,35 +45,44 @@ class Ui_MainWindow(object):
         self.splitter.setSizePolicy(sizePolicy)
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
         self.splitter.setObjectName(_fromUtf8("splitter"))
+
         self.graphicsView = QtGui.QGraphicsView(self.splitter)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
+
         self.graphicsView.setSizePolicy(sizePolicy)
         self.graphicsView.setMouseTracking(True)
         self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
+
         self.verticalLayout.addWidget(self.splitter)
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1032, 21))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         self.menuFile = QtGui.QMenu(self.menubar)
         self.menuFile.setObjectName(_fromUtf8("menuFile"))
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setSizeGripEnabled(True)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
+
         self.toolBar = QtGui.QToolBar(MainWindow)
         self.toolBar.setMouseTracking(True)
         self.toolBar.setObjectName(_fromUtf8("toolBar"))
         MainWindow.addToolBar(QtCore.Qt.LeftToolBarArea, self.toolBar)
+
         self.toolBar_2 = QtGui.QToolBar(MainWindow)
         self.toolBar_2.setObjectName(_fromUtf8("toolBar_2"))
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar_2)
+
         self.action_Open = QtGui.QAction(MainWindow)
         self.action_Open.setObjectName(_fromUtf8("action_Open"))
+
         self.menuFile.addAction(self.action_Open)
         self.menubar.addAction(self.menuFile.menuAction())
 
