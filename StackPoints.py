655ab4431d10f3c1127268f1c801cc273b40f32a
cmicek1 <cmicek325@verizon.net>
2016-11-28 15:58:31 -0500

    Points draw & scale!

--- StackPoints.py
+++ StackPoints.py
@@ -4,9 +4,10 @@ import PyQt4.QtGui as qg
 
 class DrawingPointsWidget(qg.QWidget):
     def __init__(self, browser):
-        super(qg.QWidget, self).__init__(browser)
+        super(qg.QWidget, self).__init__()
 
         # Want to pass reference to keep z up-to-date
+        self.browser = browser
         self.scaleFactor = 1  # fraction of total
         self.isVisible = 1
 
@@ -18,17 +19,16 @@ class DrawingPointsWidget(qg.QWidget):
 
         # self.setAttribute(Qt.WA_TransparentForMouseEvents);
 
-        self.setUI()
-
     def setUI(self):
 
-        self.setGeometry(0, 0, self.parent().imageLabel.width(), self.parent().imageLabel.height())
+        self.setGeometry(0, 0, self.browser.splitter.width(), self.browser.splitter.width())
         # self.setWindowTitle('Points')
         self.show()
 
     def paintEvent(self, e):
+        self.setUI()
         qp = qg.QPainter(self)
-        if self.parent().stack is not None:
+        if self.browser.stack is not None:
             self.drawPoints(qp)
 
     def drawPoints(self, qp, xfactor=None, yfactor=None, xtranslate=None, ytranslate=None):
@@ -37,29 +37,25 @@ class DrawingPointsWidget(qg.QWidget):
         if not self.isVisible:
             return
 
-        qp.setPen(qg.QPen(qc.Qt.red, 12, qc.Qt.DashDotLine, qc.Qt.RoundCap))
+        qp.setPen(qg.QPen(qc.Qt.red, 6, qc.Qt.DashDotLine, qc.Qt.RoundCap))
 
         rectWidth = 7
         rectHeight = 7
 
-        # TODO: Rewrite for current stack class
-        # xyz = self.parent().stack.nodedb.dframe.getMask(self.parent().z)
-        # xyz /= float(self.map.stackList[1].header['voxelx'])
-        # xyz *= self.scaleFactor
-        # # xyz = abs(np.random.random((20,20)) * 100)
-        # num = len(xyz)
-        # for i in range(num):
-        #     x = xyz[i][0]
-        #     y = xyz[i][1]
-        #     qp.drawEllipse(x, y, rectWidth, rectHeight)
-
         offset = 1
 
+        # Note: Might break on zoom/further manipulations; more testing necessary
+        start_scale = float(self.browser.splitter.width()) / self.browser.stack.imarray.shape[1]
+
         if xfactor is None:
-            xfactor = 1
+            xfactor = start_scale
+        else:
+            xfactor *= start_scale
 
         if yfactor is None:
-            yfactor = 1
+            yfactor = start_scale
+        else:
+            yfactor *= start_scale
 
         if xtranslate is None:
             xtranslate = 0
@@ -68,17 +64,17 @@ class DrawingPointsWidget(qg.QWidget):
             ytranslate = 0
 
         # Min slice
-        d1 = int(self.parent().z - offset)
+        d1 = int(self.browser.z - offset)
         if d1 < 0:
             d1 = 0
 
         # Max slice
-        d2 = int(self.parent().z + offset)
-        if d2 > self.parent().stack.maxz:
-            d2 = int(self.parent().stack.maxz)
-        nodes = self.parent().stack.node_db.dframe.loc[(d1 <= self.parent().stack.node_db.dframe['z']) &
-                                                       (self.parent().stack.node_db.dframe['z'] <= d2)]
+        d2 = int(self.browser.z + offset)
+        if d2 > self.browser.stack.maxz:
+            d2 = int(self.browser.stack.maxz)
+        nodes = self.browser.stack.node_db.dframe.loc[(d1 <= self.browser.stack.node_db.dframe['z']) &
+                                                       (self.browser.stack.node_db.dframe['z'] <= d2)]
         for node in nodes.itertuples():
-            qp.drawEllipse(int(node.y * xfactor / self.parent().stack.dx +
-                           xtranslate), int(node.x * yfactor / self.parent().stack.dy +
+            qp.drawEllipse(int(node.x * xfactor / self.browser.stack.dx +
+                           xtranslate), int(node.y * yfactor / self.browser.stack.dy +
                            ytranslate), rectWidth, rectHeight)
