657cf913395289731e400ee25828a3e6d08e635b
cmicek1 <cmicek325@verizon.net>
2017-01-12 18:19:25 -0500

    Finally updated documentation.

--- StackPoints.py
+++ StackPoints.py
@@ -3,7 +3,24 @@ import PyQt4.QtGui as qg
 
 
 class DrawingPointsWidget(qg.QWidget):
+    """
+    Custom QWidget class to regulate display of and user interaction with any overlays for the Stack
+    Browser's TiffStack, as defined in the text files within its home directory. The objects it regulates are custom
+    QGraphicsItems, defined below this class.
+    """
     def __init__(self, browser):
+        """
+        Initializer for the QWidget. Calls the initializer of the superclass first, then initializes custom
+        attributes. These include, among others:
+            * nodes, a dictionary of Node lists organized by z slice: dict[int z, list[Node n]]
+            * slabs, a dictionary of Slab lists organized by z slice: dict[int z, list[Slab s]]
+            * edge_segs, a dictionary of EdgeSegments organized by edge index: dict[int edgeIdx, list[EdgeSegment es]]
+
+        :param browser: The stack browser instance this widget will be a member of; passed so that all attributes are
+                        passed by reference between both, and are up-to-date.
+
+        :type browser: MainWindow
+        """
         super(qg.QWidget, self).__init__()
         self.nodes = {}
         self.slabs = {}
@@ -12,7 +29,7 @@ class DrawingPointsWidget(qg.QWidget):
         # Want to pass reference to keep z up-to-date
         self.browser = browser
         self.scaleFactor = 1  # fraction of total
-        self.isVisible = 1
+        self.isVisible = 1  # Show all points and edges by default
 
         self.setWindowFlags(self.windowFlags() | qc.Qt.FramelessWindowHint)
         self.setMouseTracking(True)
@@ -22,26 +39,59 @@ class DrawingPointsWidget(qg.QWidget):
         self.cur_scale = None
 
     def setUI(self):
+        """
+        Sets the widget to the appropriate size and triggers a display update to reflect the current state of all items.
+
+        :return: None
+        """
 
         self.setGeometry(0, 0, self.browser.splitter.width(), self.browser.splitter.width())
-        # self.setWindowTitle('Points')
         self.show()
 
-    def paintEvent(self, e):
+    def paintEvent(self, event):
+        """
+        Overloaded function from QWidget. Receives QPaintEvents for the widget and calls 'setUI' to display all items
+        correctly.
+
+        :param event: The QPaintEvent received
+
+        :type event: PyQt4.QtGui.QPaintEvent
+
+        :return: None
+        """
         self.setUI()
 
     def initPoints(self, xfactor=None, yfactor=None, xtranslate=None, ytranslate=None):
+        """
+        Initializes all of this widget's derived QGraphicsItems to their initial state, placing them all in their
+        correct positions based on the browser TiffStack's DataFrame members. All items are hidden from view until
+        the current z slice is within a specified distance of the z-coordinate of each.
 
+        :param xfactor: Scale factor for item x-coordinates (the previous scale by default)
+        :param yfactor: Scale factor for item y-coordinates (the previous scale by default)
+        :param xtranslate: Amount of pixels to translate each item in the x-direction (0 by default)
+        :param ytranslate: Amount of pixels to translate each item in the y-direction (0 by default)
+
+        :type xfactor: float
+        :type yfactor: float
+        :type xtranslate: int
+        :type ytranslate: int
+
+        :return: None
+        """
+
+        # Create pens and brushes to color nodes, slabs, and edges
         slab_pen = qg.QPen(qc.Qt.cyan, 4, qc.Qt.SolidLine, qc.Qt.RoundCap)
         slab_brush = qg.QBrush(qc.Qt.cyan)
 
         node_edge_pen = qg.QPen(qc.Qt.red, 1, qc.Qt.SolidLine, qc.Qt.RoundCap)
         node_edge_brush = qg.QBrush(qc.Qt.red)
 
+        # Size of each node/slab's bounding box
         rectWidth = 7
         rectHeight = 7
 
-        # Note: Might break on zoom/further manipulations; more testing necessary
+        # Current scale of all items, determined by the size of the image container relative to the image
         self.start_scale = float(self.browser.splitter.width()) / self.browser.stack.imarray.shape[1]
         self.cur_scale = self.start_scale
 
@@ -61,6 +111,7 @@ class DrawingPointsWidget(qg.QWidget):
         if ytranslate is None:
             ytranslate = 0
 
+        # Place all items in the correct position, and store them in dicts
         time = 0
         prev_slab = None
         prev_xpos = 0
@@ -69,7 +120,9 @@ class DrawingPointsWidget(qg.QWidget):
             s = self.Slab(0.0, 0.0, rectWidth, rectHeight, dfentry=slab)
             xpos = slab.x * xfactor / self.browser.stack.dx + xtranslate
             ypos = slab.y * yfactor / self.browser.stack.dy + ytranslate
-            s.setPos(xpos - rectWidth/2, ypos - rectHeight/2)
+            s.setPos(xpos - rectWidth/2, ypos - rectHeight/2)  # Note: Coordinates for QGraphicsEllipseItems are the
+                                                               # upper-left corner of the item's bounding box,
+                                                               # so need to translate to center
             s.setPen(slab_pen)
             s.setBrush(slab_brush)
             s.setZValue(s.zValue() + 1)
@@ -81,6 +134,7 @@ class DrawingPointsWidget(qg.QWidget):
                 self.slabs[slab.z].append(s)
             self.browser.scene.addItem(s)
 
+            # Connect each Slab that has a neighbor of the same edge index with an EdgeSegment
             if time == 0:
                 prev_slab = s
                 prev_xpos = xpos
@@ -107,6 +161,7 @@ class DrawingPointsWidget(qg.QWidget):
                 prev_ypos = ypos
                 time += 1
 
+        # Change pen to Node specs
         node_edge_pen.setWidth(7)
 
         for node in self.browser.stack.node_db.dframe.itertuples():
@@ -125,19 +180,33 @@ class DrawingPointsWidget(qg.QWidget):
             self.browser.scene.addItem(n)
 
     def drawPoints(self, resize=False):
+        """
+        Function to determine which items to show in the current browser view. Checks for Nodes and Slabs in the
+        current z slice +/- an offset value (1 slice by default), and EdgeSegments that have any visible endpoints,
+        and displays them. Also rescales all items if the window is resized.
 
+        :param resize: A boolean indicating if the call for this function occurred during a resize event.
+
+        :type resize: bool
+
+        :return: None
+        """
+
+        # If overlay is not visible, do nothing
         if not self.isVisible:
             return
 
+        # Else, set offset from current z to search for items to be shown
         offset = 1
 
-        # Min slice
+        # Calculate min slice in range
         d1 = int(self.browser.z - offset)
         prev = d1 - 1
         if d1 < 0:
             d1 = 0
             prev = None
 
+        # If scrolling down, hide items in previous uppermost visible slice
         if prev is not None:
             if prev in self.slabs:
                 for s in self.slabs[prev]:
@@ -151,13 +220,14 @@ class DrawingPointsWidget(qg.QWidget):
                 for n in self.nodes[prev]:
                     n.hide()
 
-        # Max slice
+        # Calculate max slice of range
         d2 = int(self.browser.z + offset)
         nxt = d2 + 1
         if d2 > self.browser.stack.maxz:
             d2 = int(self.browser.stack.maxz)
             nxt = None
 
+        # If scrolling up, hide items in previous lowermost visible slice
         if nxt is not None:
             if nxt in self.slabs:
                 for s in self.slabs[nxt]:
@@ -171,13 +241,16 @@ class DrawingPointsWidget(qg.QWidget):
                 for n in self.nodes[nxt]:
                     n.hide()
 
+        # Check scale out of scope so the current stored scale can be modified
         scale = float(self.browser.splitter.width()) / self.browser.stack.imarray.shape[1]
-        if resize:
+        if resize:  # Scale all items (both visible and invisible) via a linear iteration through each member dict,
+                    # so scales of all items are easy to keep track of
             for k in self.slabs:
                 for s in self.slabs[k]:
                     s.setPos(s.pos() / self.cur_scale * scale)
             for idx in self.edge_segs:
-                # Handle error case of edge with one slab
+                # Handle error case of edge with one slab (shouldn't occur, but can if there was an error in the
+                # creation of the initial text file)
                 try:
                     for es in self.edge_segs[idx]:
                         temp = es.line()
@@ -212,6 +285,10 @@ class DrawingPointsWidget(qg.QWidget):
         self.cur_scale = scale
 
     class Node(qg.QGraphicsEllipseItem):
+        """
+        Class for nodes in the scene derived from QGraphicsEllipseItem. Useful for event handling and linking each
+        item to its corresponding DataFrame entry.
+        """
         def __init__(self, *_args, **kwargs):
             super(qg.QGraphicsEllipseItem, self).__init__(*_args)
             self.setFlag(qg.QGraphicsItem.ItemIsSelectable)
@@ -223,9 +300,14 @@ class DrawingPointsWidget(qg.QWidget):
             tempop = option
             tempop.state &= not qg.QStyle.State_Selected
             # TODO: Change painter here for selection
+            # painter = qg.QGraphicsItem
             qg.QGraphicsEllipseItem.paint(self, painter, tempop, widget)
 
     class Slab(qg.QGraphicsEllipseItem):
+        """
+        Class for slabs in the scene derived from QGraphicsEllipseItem. Useful for event handling and linking each
+        item to its corresponding DataFrame entry.
+        """
         def __init__(self, *_args, **kwargs):
             super(qg.QGraphicsEllipseItem, self).__init__(*_args)
             self.setFlag(qg.QGraphicsItem.ItemIsSelectable)
@@ -234,6 +316,10 @@ class DrawingPointsWidget(qg.QWidget):
                 self.dfentry = kwargs['dfentry']
 
     class EdgeSegment(qg.QGraphicsLineItem):
+        """
+        Class for edge segments in the scene derived from QGraphics:LineItem. Holds each segment's edge index ans its
+        two Slab endpoints.
+        """
         def __init__(self, *_args, **kwargs):
             super(qg.QGraphicsLineItem, self).__init__(*_args)
             self.setFlag(qg.QGraphicsItem.ItemIsSelectable)
--- stackbrowserQt.py
+++ stackbrowserQt.py
@@ -11,26 +11,38 @@ import tkFileDialog
 
 
 class MainWindow(qg.QMainWindow):
+    """
+    Main application window for Qt Stack Browser. Subclasses QMainWindow, and serves as the top-level user interface
+    for the application. It handles widget layout, file I/O, display, and user input.
+    """
     def __init__(self):
-        super(MainWindow, self).__init__()
-        setter = ui.Ui_MainWindow()
+        """
+        Initializer for the Main Window. First calls QMainWindow initializer, then creates layout and UI with help
+        from the Ui_MainWindow class in stackbrowserQtUI.py, exported from Qt Designer.
+        """
+        super(qg.QMainWindow, self).__init__()  # Default initializer for QMainWindow called first
+        setter = ui.Ui_MainWindow()  # Tool for setting UI
         ui.Ui_MainWindow.setupUi(setter, self)
         action_open = setter.action_Open
 
         action_open.triggered.connect(lambda: self.action_handler('open'))
 
+        # Add additional relevant attributes
         self.stack = None
         self.z = None
         self.scale = 1.0
         self.image = None
         self.COLORTABLE = []
+        # Set default color table for 8-bit images
         for i in range(256):
             self.COLORTABLE.append(qg.qRgb(i / 4, i, i / 2))
 
         self.splitter = setter.splitter
 
+        # Initialize the scene and the view of the application, the two most-often used attributes
         self.scene = qg.QGraphicsScene()
-        self.view = _MyGraphicsView(setter.graphicsView)
+        self.view = _MyGraphicsView(setter.graphicsView)  # Uses hidden class derived from QGraphicsView to suppress
+                                                          # default scrolling behavior
 
         self.view.setScene(self.scene)
         self.view.fitInView(self.scene.sceneRect(), qc.Qt.KeepAspectRatio)
@@ -38,8 +50,9 @@ class MainWindow(qg.QMainWindow):
         self.view.setHorizontalScrollBarPolicy(qc.Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(qc.Qt.ScrollBarAlwaysOff)
 
-        self.view.keyPressEvent = self.keyPressEvent
+        self.view.keyPressEvent = self.keyPressEvent  # Pass top-level keyboard events to view
 
+        # Create container for the image to be displayed (easy with a QLabel)
         self.imageLabel = qg.QLabel()
         self.imageLabel.setSizePolicy(qg.QSizePolicy.Ignored, qg.QSizePolicy.Ignored)
         self.imageLabel.setScaledContents(True)
@@ -51,9 +64,11 @@ class MainWindow(qg.QMainWindow):
 
         self.scene.addWidget(self.imageLabel)
 
+        # Add custom widget handling display of and interaction with any items overlaying the image
         self.points = sps.DrawingPointsWidget(self)
         self.scene.addWidget(self.points)
 
+        # Add removable toolbars for useful interaction and information display
         self.leftToolbar = setter.toolBar
         self.topToolbar = setter.toolBar_2
 
@@ -70,16 +85,43 @@ class MainWindow(qg.QMainWindow):
         self.leftToolbar.addWidget(self.list)
 
     def action_handler(self, handle, *args):
+        """
+        Custom universal handler for valid hidden functions. Uses a dictionary associating keywords with their
+        respective functions, and passes along necessary arguments for execution. Useful for both utility functions
+        and Qt signals and slots.
+
+        :param handle: Keyword for the desired function
+        :param args: Arguments to be passed to the function named by 'handle' (the function must accept *args as a
+                     parameter, even if it is unused
+
+        :type handle: str
+        :type args: list
+
+        :return: None
+        """
         valid_funcs = {'open': self._open, 'pan': self._pan, 'zoom': self._zoom}
 
         valid_funcs[handle](args)
 
-    def eventFilter(self, source, event):
+    def eventFilter(self, watched, event):
+        """
+        Overloaded method from QMainWindow. Catches the event from watched and allows it to proceed if it is of the
+        appropriate type.
+
+        :param watched: The source (widget) of the caught event
+        :param event: The event that was caught
+
+        :type watched: PyQt4.QtCore.QObject
+        :type event: PyQt4.QtCore.QEvent
+
+        :return: True if 'event' was filtered out, False else
+        :rtype: bool
+        """
         # TODO: Detect when toolbar is moved, and reposition points appropriately
         if event.type() == qc.QEvent.Wheel or event.type() == qc.QEvent.GraphicsSceneWheel:
             self.wheelEvent(event)
 
-        if event.type() == qc.QEvent.MouseMove and source is self.view.viewport():
+        if event.type() == qc.QEvent.MouseMove and watched is self.view.viewport():
             pos = event.pos()
             print('mouse move: (%d, %d)' % (pos.x(), pos.y()))
             # self.updateStatus('mouse ' + str(pos.x()) + ' ' + str(pos.y()))
@@ -91,16 +133,40 @@ class MainWindow(qg.QMainWindow):
         if event.type() == qc.QEvent.Resize:
             self.resizeEvent(event)
 
-        return qg.QMainWindow.eventFilter(self, source, event)
+        # After taking custom action, send back to superclass event filter for default behavior
+        return qg.QMainWindow.eventFilter(self, watched, event)
 
     def showEvent(self, event):
+        """
+        Overloaded method from QMainWindow. Handles QShowEvents, which occur when the application window is shown on
+        the screen.
+
+        :param event: The QShowEvent received
+
+        :type event: PyQt4.QtGui.QShowEvent
+
+        :return: None
+        """
+
+        # Simply resizes widgets contained in the application to be of the appropriate size when the window is first
+        # shown.
         size = self.splitter.size()
         self.view.resize(size)
         self.imageLabel.resize(size)
         print(self.imageLabel.size())
 
     def resizeEvent(self, event):
+        """
+        Overloaded method from QMainWindow. Handles QResizeEvents when the window is resized.
+
+        :param event: The QResizeEvent received
+
+        :type event: PyQt4.QtGui.QResizeEvent
+
+        :return: None
+        """
         if self.stack is not None:
+            # Resize image and overlay to new size
             # Add drawing nodes to window display functions
             a = self.stack.get_slice(self.z)
             self.image = qg.QImage(a.tostring(), a.shape[0], a.shape[1], qg.QImage.Format_Indexed8)
@@ -113,10 +179,22 @@ class MainWindow(qg.QMainWindow):
             # Now draw new nodes
             self.points.drawPoints(resize=True)
         else:
+            # Resize container widgets to new window size
             self.view.resize(self.width(), self.width())
             self.imageLabel.resize(self.width(), self.width())
 
     def wheelEvent(self, event):
+        """
+        Overloaded method from QMainWindow. Handles QWheelEvents when the mouse wheel is scrolled and the resulting
+        event is accepted.
+
+        :param event: The accepted QWheelEvent
+
+        :type event: PyQt4.QtGui.QWheelEvent
+
+        :return: None
+        """
+        # If stack is loaded, view next slice in scroll direction within the valid range.
         if self.stack is not None:
             # Add drawing nodes to window display functions
             self.z -= np.sign(event.delta())
@@ -135,9 +213,19 @@ class MainWindow(qg.QMainWindow):
             self.points.drawPoints()
 
     def keyPressEvent(self, event):
-        # print 'window.keyPressEvent:', event.text()
-        # use QLabel.setGeometry(h,v,w,h) to pan the image
-        # print 'keyPressEvent()'
+        """
+        Overloaded method from QMainWindow. Handles QKeyEvents when a key is pressed and the resulting event is
+        accepted.
+
+        :param event: The accepted QKeyEvent
+
+        :type event: PyQt4.QtGui.QKeyEvent
+
+        :return: None
+                """
+
+        # Values for amount of pixels to pan by (wrt original image size) and factor to zoom by. Hard coded for now,
+        # but the plan is to have these as tunable options later, if desired.
         panvalue = 20
         zoomfactor = 1.5
         k = event.key()
@@ -149,6 +237,19 @@ class MainWindow(qg.QMainWindow):
             self.action_handler('zoom', k, zoomfactor)
 
     def _open(self, args):
+        """
+        Hidden function invoked by the action handler that opens and displays a TiffStack. Called by pressing 'Open'
+        in the 'File' menu of the GUI.
+
+        :param args: Unused, but required in the function signature for it to be handled correctly by the action
+                     handler.
+
+        :type args: None
+
+        :return: None
+        """
+        # Open a dialog box allowing the user to select a TiffStack to display. See tiffstack.py and the repository
+        # README for more info.
         root = Tk.Tk()
         root.attributes('-topmost', True)
         root.withdraw()
@@ -156,6 +257,7 @@ class MainWindow(qg.QMainWindow):
             initialdir=os.path.expanduser('~/Desktop'))
         self.stack = ts.TiffStack(fpath)
 
+        # Start at slice 0
         self.z = 0
         a = self.stack.get_slice(0)
         self.image = qg.QImage(a.tostring(), a.shape[0], a.shape[1], qg.QImage.Format_Indexed8)
@@ -164,50 +266,73 @@ class MainWindow(qg.QMainWindow):
         self.imageLabel.setPixmap(p.scaled(self.imageLabel.width(), self.imageLabel.width()))
         pointModel = pt.PointTable(self.stack.node_db.dframe)
         self.list.setModel(pointModel)
+
+        # Create initial overlay and internal representation of graph data
         self.points.initPoints()
         self.points.drawPoints()
-        # self.view.fitInView(self.scene.sceneRect(), qc.Qt.KeepAspectRatio)
 
     def _pan(self, args):
+        """
+        Hidden function invoked by the action handler that pans the current view of the open TiffStack. Called by
+        pressing any of the arrow keys; the view will pan in the respective direction. Also moves the view back to
+        center when the Enter or Return keys are pressed.
+
+        :param args: First argument is the key from the QKeyEvent caught by 'keyPressEvent'; second is the
+                     number of pixels to pan by wrt the initial size of the image
+
+        :type args: list[int, int]
+
+        :return: None
+        """
         key = args[0]
         panvalue = args[1]
         if self.stack is not None:
             if key == qc.Qt.Key_Left:
-                # self.imageX -= panvalue
                 self.view.move(self.view.x() - panvalue, self.view.y())
             if key == qc.Qt.Key_Right:
-                # self.imageX += panvalue
                 self.view.move(self.view.x() + panvalue, self.view.y())
             if key == qc.Qt.Key_Up:
-                # self.imageY -= panvalue
                 self.view.move(self.view.x(), self.view.y() - panvalue)
             if key == qc.Qt.Key_Down:
-                # self.imageY += panvalue
                 self.view.move(self.view.x(), self.view.y() + panvalue)
             if key == qc.Qt.Key_Enter or key == qc.Qt.Key_Return:
-                print 'reset image to full view and center'
+                print('reset image to full view and center')
                 self.view.move(0, 0)
 
     def _zoom(self, args):
+        """
+        Hidden function invoked by the action handler that zooms the current view of the open TiffStack with respect
+        to the current mouse position, or returns the view to its initial size. Called by pressing the plus, minus, and
+        Enter/Return keys to zoom in, out, and to the initial scale, respectively.
+
+        :param args: First argument is the key from the QKeyEvent caught by 'keyPressEvent'; second is the
+                     percentage of the current view size to zoom by
+
+        :type args: list[int, float]
+
+        :return: None
+        """
         key = args[0]
         factor = args[1]
+        # Qt's default implementation of transformations with respect to the mouse position only works correctly if
+        # the view is not a derived class with the scroll disabled, so the code below is a bit of a hacky workaround.
+        # It maps the global position of the mouse to scene coordinates, scales the view appropriately, then finds
+        # the scene coordinates of the new mouse position and translates the view so the old and new positions are
+        # coincident.
         old_pos = self.view.mapToScene(self.view.mapFromGlobal(qg.QCursor.pos()))
-        if key == qc.Qt.Key_Plus:
+        if key == qc.Qt.Key_Plus:  # Zoom in
             self.scale *= factor
-            # self.view.setTransformationAnchor(qg.QGraphicsView.AnchorUnderMouse)
             self.view.scale(factor, factor)
             new_pos = self.view.mapToScene(self.view.mapFromGlobal(qg.QCursor.pos()))
             delta = new_pos - old_pos
             self.view.translate(delta.x(), delta.y())
-        if key == qc.Qt.Key_Minus:
+        if key == qc.Qt.Key_Minus:  # Zoom out
             self.scale /= factor
-            # self.view.setTransformationAnchor(qg.QGraphicsView.AnchorUnderMouse)
             self.view.scale(1.0/factor, 1.0/factor)
             new_pos = self.view.mapToScene(self.view.mapFromGlobal(qg.QCursor.pos()))
             delta = new_pos - old_pos
             self.view.translate(delta.x(), delta.y())
-        if key == qc.Qt.Key_Enter or key == qc.Qt.Key_Return:
-            # self.view.setTransformationAnchor(qg.QGraphicsView.AnchorUnderMouse)
+        if key == qc.Qt.Key_Enter or key == qc.Qt.Key_Return:  # Return to initial size
             self.view.scale(1.0/self.scale, 1.0/self.scale)
             new_pos = self.view.mapFromScene(0, 0)
             delta = -new_pos
@@ -219,16 +344,48 @@ class _MyGraphicsView(qg.QGraphicsView):
     """
     Empty abstract class; suppresses default QGraphicsView scroll behavior.
     """
-    def scrollContentsBy(self, a, b):
-        # Apparently this effects the transformation anchor
+    def __init__(self, uiview):
+        """
+        Initializer for the derived class; initializes the QGraphicsView from the UI setter using the default
+        initializer of QGraphicsView
+
+        :param uiview: QGraphicsView to initialize; should be from UI made with Qt Designer
+
+        :type uiview: PyQt4.QtGui.QGraphicsView
+        """
+        super(qg.QGraphicsView, self).__init__(uiview)
+
+    def scrollContentsBy(self, dx, dy):
+        """
+        Overloaded method of QAbstractScroll area, of which QGraphicsView is derived. Determines how much the current
+        view should scroll when its scroll bars are moved by dx and dy. This implementation does nothing because
+        the scrolling behavior of a QGraphicsView enabled by default conflicts with the intuitive scrolling by z slice
+        of the application.
+
+        :param dx: Number of pixels in the x direction the scroll bars have moved
+        :param dy: Number of pixels in the y direction the scroll bars have moved
+
+        :type dx: int
+        :type dy: int
+
+        :return: None
+        """
+        # Apparently this affects the transformation anchor
         pass
 
 
 def _exit_handler():
+    """
+    Signal connected to the top-level application class (QApplication) to terminate the program correctly in Python,
+    as PyQt4 handles Python garbage collection incorrectly.
+
+    :return: None
+    """
     sys.exit(0)
 
 
 def main():
+
     app = qg.QApplication(sys.argv)
     window = MainWindow()
     window.show()
